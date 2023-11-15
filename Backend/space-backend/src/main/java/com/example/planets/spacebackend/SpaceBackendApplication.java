98182eb62819219f7d7345345d61f604ed636606
SubZeRoy9 <turnerjroy@gmail.com>
2023-11-15 10:47:18 -0500

    Added Springboot section

---
+++ Backend/space-backend/src/main/java/com/example/planets/spacebackend/SpaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.planets.spacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBackendApplication.class, args);
+	}
+
+}
